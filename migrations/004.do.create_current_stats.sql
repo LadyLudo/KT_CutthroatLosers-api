68334087756c1936c1afe9314191232ffb137b2f
Catherine Davis <catherinevtdavis@gmail.com>
2020-08-22 18:32:11 -0600

    fourth migration

---
+++ migrations/004.do.create_current_stats.sql
@@ -0,0 +1,9 @@
+CREATE TABLE current_stats (
+    user_id INTEGER NOT NULL,
+    current_weight NUMERIC NOT NULL,
+    goal_weight NUMERIC NOT NULL,
+    display_name TEXT NOT NULL,
+    contest_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (contest_id) REFERENCES contests(contest_id)
+);
