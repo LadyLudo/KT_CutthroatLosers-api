f94c8e58c4b4a24c0013e1453e13bd3a1bf8020a
Kevin Davis <Kevindavis5454@gmail.com>
2020-09-10 15:36:06 -0600

    Revert "fixed tests"
    This reverts commit 2e4c1da16422081965db7bb68fcc9e8753850fa1.

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -91,6 +91,7 @@ describe('POST /api/users', () => {
             .send(newUser)
             .expect(201)
             .expect(res => {
+                expect(res.body.password).to.eql(newUser.password)
                 expect(res.body.display_name).to.eql(newUser.display_name)
                 expect(res.body.username).to.eql(newUser.username)
             })
@@ -160,7 +161,7 @@ describe('DELETE /api/users/:user_id', () => {
     })
 })
 
-describe('PATCH /api/users/:user_id', () => {
+describe.only('PATCH /api/users/:user_id', () => {
     context('Given no users', () => {
         it('resonds with 404', () => {
             const user_id = 123456
@@ -197,7 +198,7 @@ describe('PATCH /api/users/:user_id', () => {
                 .then(res => 
                     supertest(app)
                         .get(`/api/users/${idToUpdate}`)
-                        .expect(200)
+                        .expect(expectedUser)
                 )
         })
 
@@ -233,7 +234,7 @@ describe('PATCH /api/users/:user_id', () => {
                 .then(res => 
                     supertest(app)
                         .get(`/api/users/${idToUpdate}`)
-                        .expect(200)
+                        .expect(expectedUser)
                 )
         })
     })
@@ -299,6 +300,15 @@ describe('GET /api/users/userAuth', () => {
                 .get(`/api/users/login/userAuth`)
                 .query({ username: username, password: password})
                 .expect(200)
+        })
+
+        it('Responds with 200 and the message if passwords do not match', () => {
+            const username = 'john@gmail.com'
+            const password = 'te123'
+            return supertest(app)
+                .get(`/api/users/login/userAuth`)
+                .query({ username: username, password: password})
+                .expect(200, 'password does not match')
         })
         
     })
