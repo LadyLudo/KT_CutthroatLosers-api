0e6add91a80b368059c6e29d273adcb63c3d3475
Kevin Davis <Kevindavis5454@gmail.com>
2021-07-13 14:54:17 -0700

    testingendpoints

--- src/points/points-service.js
+++ src/points/points-service.js
@@ -1,82 +1,62 @@
 const PointsService = {
-    getAllPoints(knex) {
-      return knex
-        .select('*')
-        .from('points')
-        .orderBy('date_created')
-    },
-  
-    insertPoints(knex, newPoints) {
-      return knex
-        .insert(newPoints)
-        .into('points')
-        .returning('*')
-        .then(rows => {
-          return rows[0]
-        })
-    },
-  
-    getByUserId(knex, user_id) {
-      return knex
-        .from('points')
-        .select('*')
-        .where('user_id', user_id)   
-    },
+  getAllPoints(knex) {
+    return knex.select().from("points").orderBy("date_created");
+  },
 
-    getByContestId(knex, contest_id) {
-        return knex
-          .from('points')
-          .select('*')
-          .where('contest_id', contest_id)
-      },
+  insertPoints(knex, newPoints) {
+    return knex
+      .insert(newPoints)
+      .into("points")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
 
-    getById(knex, id) {
-        return knex
-          .from('points')
-          .select('*')
-          .where('id', id)
-          .first()
-      },
+  getByUserId(knex, user_id) {
+    return knex.from("points").select("*").where("user_id", user_id);
+  },
 
-    getTotalUserPoints(knex, user_id, contest_id) {
-        return knex
-          .from('points')
-          .sum('points')
-          .where({ user_id, contest_id })
-    },
+  getByContestId(knex, contest_id) {
+    return knex.from("points").select("*").where("contest_id", contest_id);
+  },
 
-    getUserStomachPoints(knex, user_id, contest_id) {
-      return knex
-        .from('points')
-        .sum('points')
-        .where({ user_id: user_id, contest_id: contest_id, category: 'stomach' })
-    },
+  getById(knex, id) {
+    return knex.from("points").select("*").where("id", id).first();
+  },
 
-    getUserWeightPoints(knex, user_id, contest_id) {
-      return knex
-        .from('points')
-        .sum('points')
-        .where({ user_id: user_id, contest_id: contest_id, category: 'weight' })
-    },
+  getTotalUserPoints(knex, user_id, contest_id) {
+    return knex.from("points").sum("points").where({ user_id, contest_id });
+  },
 
-    getUserWorkoutPoints(knex, user_id, contest_id) {
-      return knex
-        .from('points')
-        .sum('points')
-        .where({ user_id: user_id, contest_id: contest_id, category: 'workout' })
-    },
-  
-    deletePoints(knex, id) {
-      return knex('points')
-        .where({ id })
-        .delete()
-    },
-  
-    updatePoints(knex, id, newPointsFields) {
-      return knex('points')
-        .where({ id })
-        .update(newPointsFields)
-    },
-  }
-  
-  module.exports = PointsService
+  getUserStomachPoints(knex, user_id, contest_id) {
+    return knex
+      .from("points")
+      .sum("points")
+      .where({ user_id: user_id, contest_id: contest_id, category: "stomach" });
+  },
+
+  getUserWeightPoints(knex, user_id, contest_id) {
+    return knex
+      .from("points")
+      .sum("points")
+      .where({ user_id: user_id, contest_id: contest_id, category: "weight" });
+  },
+
+  getUserWorkoutPoints(knex, user_id, contest_id) {
+    return knex
+      .from("points")
+      .sum("points")
+      .where({ user_id: user_id, contest_id: contest_id, category: "workout" });
+  },
+
+  deletePoints(knex, id) {
+    return knex("points").where({ id }).delete();
+  },
+
+  updatePoints(knex, id, newPointsFields) {
+    return knex("points").where({ id }).update(newPointsFields);
+  },
+};
+
+module.exports = PointsService;
